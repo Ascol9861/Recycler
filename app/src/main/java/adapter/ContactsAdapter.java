b5e6062eb8f0759369a639383da1bb6a6d75fdbf
Softwarica College Lab <softwarica@softwarica.com>
2019-04-18 11:51:54 +0545

    contacts adapter created

--- app/src/main/java/adapter/ContactsAdapter.java
+++ app/src/main/java/adapter/ContactsAdapter.java
@@ -1,15 +1,45 @@
 package adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
-
 import com.softwarica.recycler.R;
 
-import de.hdodenhof.circleimageview.CircleImageView;
+import java.util.List;
 
-public class ContactsAdapter {
+import de.hdodenhof.circleimageview.CircleImageView;
+import model.Contacts;
+
+public class ContactsAdapter  extends RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder>{
+
+    Context mContext;
+    List<Contacts> contactsList;
+
+    public ContactsAdapter(Context mContext, List<Contacts> contactsList) {
+        this.mContext = mContext;
+        this.contactsList = contactsList;
+    }
+
+
+    @NonNull
+    @Override
+    public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsViewHolder contactsViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class ContactsViewHolder extends RecyclerView.ViewHolder {
 
         CircleImageView imgProfile;
