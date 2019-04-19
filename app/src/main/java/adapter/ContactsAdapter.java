8db4fefb85316945cffb64d164f4872f0b5e8c06
Softwarica College Lab <softwarica@softwarica.com>
2019-04-19 07:57:01 +0545

    new activity added

--- app/src/main/java/adapter/ContactsAdapter.java
+++ app/src/main/java/adapter/ContactsAdapter.java
@@ -1,12 +1,15 @@
 package adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.softwarica.recycler.DetailsActivity;
 import com.softwarica.recycler.R;
 
 import java.util.List;
@@ -34,10 +37,23 @@ public class ContactsAdapter  extends RecyclerView.Adapter<ContactsAdapter.Conta
 
     @Override
     public void onBindViewHolder(@NonNull ContactsViewHolder contactsViewHolder, int i) {
-        Contacts contacts = contactsList.get(i);
+        final Contacts contacts = contactsList.get(i);
         contactsViewHolder.imgProfile.setImageResource(contacts.getImageId());
         contactsViewHolder.tvName.setText(contacts.getName());
         contactsViewHolder.tvPhone.setText(contacts.getPhoneNo());
+
+        contactsViewHolder.imgProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, DetailsActivity.class);
+
+                intent.putExtra("image", contacts.getImageId());
+                intent.putExtra("name", contacts.getName());
+                intent.putExtra("phone", contacts.getPhoneNo());
+
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
